c14820592751f11e0b3ae8827ab173890e7adb86
Maxime Arthaud <maxime@arthaud.me>
2014-10-11 12:25:43 +0200

    Add test1.c

---
+++ tests/test1.c
@@ -0,0 +1,21 @@
+int c = 'c';
+int point = '.';
+bool b = false;
+
+b = 'a' <= c && c <= 'z';
+
+if(!b) {
+    write_char('e');
+    write_char('r');
+    write_char('r');
+    write_char('o');
+    write_char('r');
+}
+else {
+    int e = 'o';
+    int f = 'k';
+    write_char(e);
+    write_char(f);
+}
+    
+write_char(point);
